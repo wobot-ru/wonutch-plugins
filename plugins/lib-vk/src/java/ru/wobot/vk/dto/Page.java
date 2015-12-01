7cb737431116887eee7edc108481da8a8260731b
Kviz <kviz@outlook.com>
2015-12-01 19:28:04 +0300

    преобразую batch комментов в структуру понятную IndexWriter для индексации одной страницы, как множества документов

---
+++ plugins/lib-vk/src/java/ru/wobot/vk/dto/Page.java
@@ -0,0 +1,11 @@
+package ru.wobot.vk.dto;
+
+public class Page {
+    public final String url;
+    public final String content;
+
+    public Page(String url, String content) {
+        this.url = url;
+        this.content = content;
+    }
+}
