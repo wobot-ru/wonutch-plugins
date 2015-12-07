25501e683248afbae53bf433b393f58570f951d3
Kviz <kviz@outlook.com>
2015-12-07 18:55:26 +0300

    WB-8 #close

--- plugins/lib-vk/src/java/ru/wobot/vk/dto/Page.java
+++ plugins/lib-vk/src/java/ru/wobot/vk/dto/Page.java
@@ -12,7 +12,7 @@ public class Page {
         this.url = url;
         this.title = title;
         this.content = content;
-        //todo: продумать как зделать по уму. сейчас у нас content содержит title. но в общем случае это не так.
+        //todo: Продумать как сделать по уму. Сейчас у нас content содержит title, но в общем случае это не так.
         this.digest = DigestUtils.md5Hex(title + content);
     }
 }
