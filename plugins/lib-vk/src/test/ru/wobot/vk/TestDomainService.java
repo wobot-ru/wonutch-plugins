4f1839c9ad3478fa6069b661667751e14d361f62
lmisakyan <l.misakyan@gmail.com>
2015-12-15 20:01:57 +0300

    Exception handling changes.

--- plugins/lib-vk/src/test/ru/wobot/vk/TestDomainService.java
+++ plugins/lib-vk/src/test/ru/wobot/vk/TestDomainService.java
@@ -4,11 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.social.vkontakte.api.VKontakteErrorException;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class TestDomainService {
     @Test(expected = VKontakteErrorException.class)
-    public void is_indexPosts_for_private_profiles_should_throw_exception() throws MalformedURLException {
+    public void is_indexPosts_for_private_profiles_should_throw_exception() throws IOException {
         Response request = DomainService.request("http://id7076373/index-posts");
 
         Assert.assertNotNull(request);
@@ -16,7 +17,7 @@ public class TestDomainService {
 
     //http://durov/index-posts
     @Test
-    public void is_indexPosts_for_durov_profile_should_return_response() throws MalformedURLException {
+    public void is_indexPosts_for_durov_profile_should_return_response() throws IOException {
         Response request = DomainService.request("http://durov/index-posts");
 
         Assert.assertNotNull(request);
