5c92be0634e7de7043a2dbac7a6e22179fb89fee
lmisakyan <l.misakyan@gmail.com>
2016-01-19 19:44:06 +0300

    Core libs moved to its own parent.

---
+++ core-libs/sm-core/src/java/ru/wobot/sm/core/domain/ParseResult.java
@@ -0,0 +1,25 @@
+package ru.wobot.sm.core.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ParseResult {
+    public final String url;
+    public final String title;
+    public final String content;
+    public final Map<String, String> links;
+    public boolean isMultiPage;
+
+    public ParseResult(String url, String title, String content, Map<String, String> links) {
+        this.url = url;
+        this.title = title;
+        this.content = content;
+        this.links = Objects.requireNonNull(links, "links argument must not be null.");
+    }
+
+    public ParseResult(String url, String title, String content, boolean isMultiPage) {
+        this(url, title, content, new HashMap<String, String>());
+        this.isMultiPage = isMultiPage;
+    }
+}
