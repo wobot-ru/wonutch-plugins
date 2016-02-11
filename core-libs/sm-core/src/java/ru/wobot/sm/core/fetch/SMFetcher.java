671592f846d26e5e9d9761a81ad69229fa42dac9
Kviz <kviz@outlook.com>
2016-02-11 18:17:40 +0300

    Remove annotations at SMFetcher.

--- core-libs/sm-core/src/java/ru/wobot/sm/core/fetch/SMFetcher.java
+++ core-libs/sm-core/src/java/ru/wobot/sm/core/fetch/SMFetcher.java
@@ -1,8 +1,6 @@
 package ru.wobot.sm.core.fetch;
 
 import ru.wobot.sm.core.domain.SMProfile;
-import ru.wobot.uri.Path;
-import ru.wobot.uri.PathParam;
 
 import java.io.IOException;
 import java.util.List;
@@ -10,21 +8,15 @@ import java.util.List;
 public interface SMFetcher {
     List<SMProfile> getProfiles(List<String> userIds) throws IOException;
 
-    @Path("{userId}/friends")
-    List<String> getFriendIds(@PathParam("userId") String userId) throws IOException;
+    List<String> getFriendIds(String userId) throws IOException;
 
-    @Path("{userId}/index-posts")
-    int getPostCount(@PathParam("userId") String userId) throws IOException;
+    int getPostCount(String userId) throws IOException;
 
-    @Path("{userId}/index-posts/x{offset}/{limit}")
-    FetchResponse getPostsData(@PathParam("userId") String userId, @PathParam("offset") long offset, @PathParam("limit") int limit) throws IOException;
+    FetchResponse getPostsData(String userId, long offset, int limit) throws IOException;
 
-    @Path("{userId}")
-    FetchResponse getProfileData(@PathParam("userId") String userId) throws IOException;
+    FetchResponse getProfileData(String userId) throws IOException;
 
-    @Path("{userId}/posts/{postId}")
-    FetchResponse getPostData(@PathParam("userId") String userId, @PathParam("postId") String postId) throws IOException;
+    FetchResponse getPostData(String userId, String postId) throws IOException;
 
-    @Path("{userId}/posts/{postId}/x{take}/{skip}")
-    FetchResponse getPostCommentsData(@PathParam("userId") String userId, @PathParam("postId") String postId, @PathParam("take") int take, @PathParam("skip") int skip) throws IOException;
+    FetchResponse getPostCommentsData(String userId, String postId, int take, int skip) throws IOException;
 }
