2d1fdeee32540b12b79b64e06774e97ef8ae0d40
Kviz <kviz@outlook.com>
2016-02-11 11:59:13 +0300

    Test bug in the test. This behavior is by design. We cannot guarantee the predetermined order of methods uses reflection.

--- core-libs/sm-core/src/test/ru/wobot/uri/UriTranslator_Translate_SMScheme_Test.java
+++ core-libs/sm-core/src/test/ru/wobot/uri/UriTranslator_Translate_SMScheme_Test.java
@@ -23,7 +23,7 @@ public class UriTranslator_Translate_SMScheme_Test {
     public void add_when_translate_const_segment_than_defined_method_should_be_invoked() throws InvocationTargetException, IllegalAccessException {
         // given
         // when
-        translator.translate(ParsedUri.parse("sm://root"));
+        translator.translate(ParsedUri.parse("sm://root/brood"));
 
         // than
         verify(scheme).root();
@@ -61,7 +61,7 @@ public class UriTranslator_Translate_SMScheme_Test {
 
     @Scheme("sm")
     public interface SMScheme {
-        @Path("root")
+        @Path("root/brood")
         String root();
 
         @Path("{host}/{arg1}/{arg2}")
