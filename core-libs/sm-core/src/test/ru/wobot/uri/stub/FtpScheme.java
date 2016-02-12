ce9ec2ac3d65a3ca1ee3b7f25076333b273be6cf
Kviz <kviz@outlook.com>
2016-02-12 16:53:56 +0300

    Fix UriTranslator_Translate_Test.

--- core-libs/sm-core/src/test/ru/wobot/uri/stub/FtpScheme.java
+++ core-libs/sm-core/src/test/ru/wobot/uri/stub/FtpScheme.java
@@ -1,19 +1,10 @@
 package ru.wobot.uri.stub;
 
 import ru.wobot.uri.Path;
-import ru.wobot.uri.PathParam;
 import ru.wobot.uri.Scheme;
-
-import java.lang.annotation.Inherited;
-import java.util.Objects;
 
 @Scheme("ftp")
 public class FtpScheme {
-    @Path("{arg1}")
-    String method(@PathParam("arg1") String arg1) {
-        return arg1;
-    }
-
     @Path("root")
     public Object root() {
         return new Object();
